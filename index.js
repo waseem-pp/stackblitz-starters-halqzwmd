2125fc713784907bc4e79240882d667756e6a3a6
waseem-pp <mohdwaseemofficial123@gmail.com>
2025-09-16 10:11:22 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })  
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err)); 
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
